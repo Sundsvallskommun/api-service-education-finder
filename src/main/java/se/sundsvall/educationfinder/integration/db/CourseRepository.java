58cc3f8fa1a1dcd558c23054b66d138547b4ed42
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-03-18 10:31:58 +0100

    UF-7448: Fix readonly repository

--- src/main/java/se/sundsvall/educationfinder/integration/db/CourseRepository.java
+++ src/main/java/se/sundsvall/educationfinder/integration/db/CourseRepository.java
@@ -1,11 +1,10 @@
 package se.sundsvall.educationfinder.integration.db;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import se.sundsvall.educationfinder.integration.db.model.CourseEntity;
 
 @CircuitBreaker(name = "courseRepository")
-public interface CourseRepository extends JpaRepository<CourseEntity, Long>, JpaSpecificationExecutor<CourseEntity> {
+public interface CourseRepository extends ReadOnlyRepository<CourseEntity, Long>, JpaSpecificationExecutor<CourseEntity> {
 }
