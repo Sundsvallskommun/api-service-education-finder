898b763e715e4877a712f3bca35baa59dfc1f54c
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-03-20 16:14:16 +0100

    UF-7540: Fix javadoc

--- src/main/java/se/sundsvall/educationfinder/integration/db/ReadOnlyRepository.java
+++ src/main/java/se/sundsvall/educationfinder/integration/db/ReadOnlyRepository.java
@@ -1,6 +1,7 @@
 package se.sundsvall.educationfinder.integration.db;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -9,12 +10,24 @@ import org.springframework.data.repository.Repository;
 @NoRepositoryBean
 public interface ReadOnlyRepository<T, I> extends Repository<T, I> {
 
-	T findById(I id);
+	/**
+	 * Returns an entity by its id.
+	 *
+	 * @param  id                       must not be {@literal null}.
+	 * @return                          the entity with the given id or {@literal Optional#empty()} if none found.
+	 * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+	 */
+	Optional<T> findById(I id);
 
+	/**
+	 * Returns all instances of the type.
+	 *
+	 * @return all entities
+	 */
 	List<T> findAll();
 
 	/**
-	 * Find all distinct values denoted by the provided type.
+	 * Returns all distinct values denoted by the provided type.
 	 *
 	 * @param  <P>
 	 * @param  type The type to find all distinct values for.
